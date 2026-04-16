b3be0cbb5a655915eae9c46d8fbf6c2204465f72
umesh1103-asus <66198986+umesh1103-asus@users.noreply.github.com>
2026-04-16 09:26:26 +0530

    Add: find-minimum-in-rotated-sorted-array - submission-3

---
+++ Data Structures & Algorithms/find-minimum-in-rotated-sorted-array/submission-3.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int findMin(vector<int> &nums) {
+        int n = nums.size();
+        return recFind(0, n-1, nums).second;
+    }
+
+    pair<bool,int> recFind(int start, int end, vector<int> &nums){ 
+        int n = nums.size();
+        if(start>end) return {false, -1};
+
+        int mid = start + (end-start+1)/2;
+        cout << mid << endl;
+        if(mid == 0){
+            if(nums[mid]<nums[mid+1]) return {true,nums[mid]};
+            else return recFind(mid+1, end, nums);
+        }
+        if(mid==n-1){
+            if(nums[mid]<nums[mid-1]) return {true,nums[mid]};
+            else return recFind(start, mid-1, nums);
+        }
+
+        if(nums[mid] < nums[mid-1] && nums[mid] < nums[mid+1]) return {true, nums[mid]};
+        pair<bool, int> res1 = recFind(start, mid-1, nums);
+        pair<bool, int> res2 = recFind(mid+1, end, nums);
+        if(res1.first && res2.first){
+            if(res1.second<res2.second) return res1;
+            return res2;
+        }
+        if(res1.first) return res1;
+        return res2;
+    }
+};
