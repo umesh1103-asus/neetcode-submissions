f59d571478a62b2d046c0c1339b8b57a3dfa5428
umesh1103-asus <66198986+umesh1103-asus@users.noreply.github.com>
2026-04-19 14:26:26 +0530

    Add: reorder-linked-list - submission-0

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-0.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        ListNode* curr = head;
+        rec(curr, curr);
+        return;
+    }
+
+    pair<ListNode*, ListNode*> rec(ListNode* curr, ListNode* currLast){
+        if(!currLast->next) {
+            return {curr, currLast};
+        }
+
+        pair<ListNode*, ListNode*> res = rec(curr, currLast->next);
+        curr = res.first;
+        ListNode* last = res.second;
+
+        if(curr==nullptr || last==nullptr) return {nullptr, nullptr};
+
+        ListNode* next = curr->next;
+
+        cout << "curr: " << curr->val << " last: " << last->val << " next: " << next->val << endl;
+
+        if(curr==last){
+            curr->next = nullptr;
+            return {nullptr, nullptr};
+        }
+        curr->next = last;
+
+        if(last==next){
+            last->next = nullptr;
+            return {nullptr, nullptr};
+        }
+        last->next = next;
+
+        cout << "returning: " << " next: " << next->val << " last: " << last->val << endl;
+
+        return {next, currLast};
+    }
+};
