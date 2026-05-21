08bb135d693e60a55880135efdf37783af2cab1f
umesh1103-asus <66198986+umesh1103-asus@users.noreply.github.com>
2026-05-22 02:17:08 +0530

    Add: course-schedule - submission-1

---
+++ Data Structures & Algorithms/course-schedule/submission-1.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        int n = numCourses;
+        vector<vector<int>> adj(n);
+        vector<int> indegree(n, 0);
+        queue<int> q;
+        for(auto dep:prerequisites) adj[dep[1]].push_back(dep[0]);
+        for(int i=0;i<n;i++){
+            for(auto next:adj[i]) indegree[next]++;
+        }
+
+        for(int i=0;i<n;i++) if(indegree[i]==0) q.push(i);
+        if(q.empty()) return false;
+        while(!q.empty()){
+            int front = q.front();
+            q.pop();
+            for(int next:adj[front]){
+                if(indegree[next]==0) return false;
+                indegree[next]--;
+                if(indegree[next]==0) q.push(next);
+            }
+        }
+
+        for(int x:indegree) if(x!=0) return false;
+
+        return q.empty();
+    }
+};
